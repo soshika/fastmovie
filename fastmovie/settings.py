23f65889d515cf8049bb4320883ac18c48c1e31d
Sobhan Shafiei <soshika1010@gmail.com>
2022-07-20 16:51:03 +0430

    change from debug to release mode

--- fastmovie/settings.py
+++ fastmovie/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1bo#4(h)52w4-3&ngs(n_(b%hcrwk(cn-bqd$h6_w2az0py41+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['fastmovie.online', 'http://45.148.120.241', '45.148.120.241', '*']
 
